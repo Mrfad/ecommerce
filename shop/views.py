1aa088c045295978dbd7ea2ceeac9dd786b960e5
Mrfad <49369275+Mrfad@users.noreply.github.com>
2021-05-06 21:05:55 +0300

    finised list view

--- shop/views.py
+++ shop/views.py
@@ -3,16 +3,16 @@ from .models import Category, Product
 
 def product_list(request, category_slug=None):
     category = None
-    categories = Category.objects.filter(available=True)
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
     if category_slug:
-        category = get_object_or_404(category, slug=category_slug)
+        category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-    context = {'category': category, 
-                'categories': categories, 
-                'products':products}
-    return render(request, 'shop/product/list.html', context)
+    return render(request,'shop/product/list.html', {'category': category, 'categories': categories, 'products': products})
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id, slug=slug, avaiable=True)
-    context = {'product': product}
-    return render(request, 'shop/product/detail.html', context)
+    product = get_object_or_404(Product,
+                                id=id,
+                                slug=slug,
+                                available=True)
+    return render(request, 'shop/product/detail.html', {'product': product})
